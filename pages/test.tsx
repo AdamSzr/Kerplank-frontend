9f92c6a90df5e93414d111b07560e70a1f951267
Adam Szreiber <adam.szr98@gmail.com>
2023-06-13 21:40:42 +0200

    Created functional board
    https://www.youtube.com/watch?v=jfYWwQrtzzY&ab_channel=WebDevSimplified

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,122 +1,79 @@
-import { DragEventHandler, useState } from 'react'
+import { CSSProperties, DragEventHandler, useEffect, useState } from 'react'
 // import FormData from 'form-data'
 
 const getItems = (count:number, prefix = `item`) =>
   Array.from( { length:count }, (v, k) => k ).map( k => `${prefix} ${k}` )
 
 
-export type DragRowProps = {name: string; defaultElements?: string[]}
 
-const DragRow = (props:DragRowProps) => {
-  const { defaultElements, name } = props
-  const [ widgets, setWidgets ] = useState<string[]>(defaultElements ?? [])
-
-  const handleOnDrag:DragEventHandler = (event, type) => {
-    if (!event.dataTransfer) return
-
-    console.log( `handleOnDrag` )
-    event.dataTransfer.setData( `widgetType`, type )
-  }
-
-  const handleOnDrop:DragEventHandler<HTMLDivElement> = e => {
-    console.log( `drag-drop` )
-    const widgetType = e.dataTransfer.getData( `widgetType` ) as string
-    console.log( `widgetType`, widgetType )
-    setWidgets([ ...widgets, widgetType ])
-  }
-
-  const handleDragOver:DragEventHandler<HTMLDivElement> = e => {
-    console.log( `drag-over` )
-    e.preventDefault()
-  }
-
-  const handleDragLeave = e => {
-    const type =  e.dataTransfer.getData( `widgetType` ) as string
-    console.log({ e, type })
-  }
-
-  const DragElement = ({ content }:{content: string}) => {
-    return <div className='widget' draggable onDragStart={e => handleOnDrag( e, `widget-${name}` )}>{content}</div>
-  }
-
-  return (
-    <div className='widgets'>
-      {/* <div className='widget' draggable onDragStart={e => handleOnDrag( e, `widget-a` )}>
-        widget a
-      </div>
-      <div className='widget' draggable onDragStart={e => handleOnDrag( e, `widget-b` )}>
-        widget b
-      </div>
-      <div className='widget' draggable onDragStart={e => handleOnDrag( e, `widget-c` )}>
-        widget c
-      </div> */}
-      <div className='page' onDrop={handleOnDrop} onDragOver={handleDragOver} onDragExit={() => console.log( `exit` )} onDragLeave={handleDragLeave} style={{ width:100, height:100, background:`red` }}>
-        {widgets.map( (w, idx) => <DragElement key={idx} content={w} /> )}
-      </div>
-    </div>
-  )
-}
-
-
-type DragEventHandler = (event:unknown, type:string) => void
 
 const UploadingComponent = () => {
 
 
-  // const [ widgets, setWidgets ] = useState<string[]>([])
-  // const [ widgets1, setWidgets1 ] = useState<string[]>([])
-  // const handleOnDrag:DragEventHandler = (event, type) => {
-  //   if (!event.dataTransfer) return
-  //   event.dataTransfer.setData( `widgetType`, type )
-  // }
+  const getDragAfterElement = (container:Element, y:number) => {
+    const draggableElemenets = Array.from( container.querySelectorAll( `.draggable:not(.dragging)` ) )
+    return draggableElemenets.reduce( (closest:any, child) => {
+      const box = child.getBoundingClientRect()
+      const offset = y - box.top - box.height / 2
+      if (offset < 0 && offset > closest.offset)
+        return { offset:offset, element:child }
 
-  // const handleOnDrop:DragEventHandler<HTMLDivElement> = e => {
-  //   console.log( `drag-drop` )
-  //   const widgetType = e.dataTransfer.getData( `widgetType` ) as string
-  //   console.log( `widgetType`, widgetType )
-  //   setWidgets([ ...widgets, widgetType ])
-  // }
+      return closest
+    }, { offset:Number.NEGATIVE_INFINITY, element:undefined } ).element
+  }
 
-  // const handleDragOver:DragEventHandler<HTMLDivElement> = e => {
-  //   console.log( `drag-over` )
-  //   e.preventDefault()
-  // }
+  useEffect( () => {
+    if (!window) return
 
+    const draggableElemenets = document.querySelectorAll( `.draggable` )
+    const containers = document.querySelectorAll( `.container` )
+
+    draggableElemenets.forEach( draggable => {
+
+      draggable.addEventListener( `dragstart`, () => {
+        draggable.classList.add( `dragging` )
+      } )
+
+      draggable.addEventListener( `dragend`, () => {
+        draggable.classList.remove( `dragging` )
+      } )
+
+    } )
+
+    containers.forEach( container => {
+
+      container.addEventListener( `dragover`, e => {
+        e.preventDefault()
+        const afterElement = getDragAfterElement( container, (e  as MouseEvent).clientY )
+        const draggable = document.querySelector( `.dragging` )!
+        if (!afterElement) {
+          container.appendChild( draggable! )
+        } else {
+          console.log({ afterElement })
+          container.insertBefore( draggable, afterElement )
+        }
+      } )
+
+    } )
+  }, [] )
 
   return (
     <>
 
-      <DragRow name="row1-" defaultElements={getItems( 3, `row1 -> ` )} />
-      <DragRow name="row2-" defaultElements={getItems( 3, `row2 -> ` )} />
-      {/* <div className='widgets'>
-        <div className='widget' draggable onDragStart={e => handleOnDrag( e, `widget-a` )}>
-          widget a
+      <div className='board' style={{ display:`flex`, gap:`10px` }}>
+        <div className='container' style={{  backgroundColor:`#9e9e9e` }}>
+          <div className='title' style={{ textAlign:`center` }}> drag-section-1</div>
+          <p className="draggable" draggable>1</p>
+          <p className="draggable" draggable>2</p>
         </div>
-        <div className='widget' draggable onDragStart={e => handleOnDrag( e, `widget-b` )}>
-          widget b
-        </div>
-        <div className='widget' draggable onDragStart={e => handleOnDrag( e, `widget-c` )}>
-          widget c
-        </div>
-        <div className='page' onDrop={handleOnDrop} onDragOver={handleDragOver} onDragLeave={e => console.log( `dragExit`, e )} style={{ width:100, height:100, background:`red` }}>
-          {widgets.map( (w, idx) => <div className='dropped-widget' draggable key={idx}>{w}</div> )}
+
+        <div className='container' style={{ backgroundColor:`#a67c7c` }}>
+          <div className='title' style={{ textAlign:`center` }}> drag-section-2</div>
+          <p className="draggable" draggable>3</p>
+          <p className="draggable" draggable>4</p>
         </div>
       </div>
 
-      <div className='widgets'>
-        <div className='widget' draggable onDragStart={e => handleOnDrag( e, `widget-a` )}>
-          widget a
-        </div>
-        <div className='widget' draggable onDragStart={e => handleOnDrag( e, `widget-b` )}>
-          widget b
-        </div>
-        <div className='widget' draggable onDragStart={e => handleOnDrag( e, `widget-c` )}>
-          widget c
-        </div>
-        <div className='page' onDrop={handleOnDrop} onDragOver={handleDragOver} style={{ width:100, height:100, background:`red` }}>
-          {widgets1.map( (w, idx) => <div className='dropped-widget' draggable key={idx}>{w}</div> )}
-        </div>
-      </div> */}
     </>
   )
 }
