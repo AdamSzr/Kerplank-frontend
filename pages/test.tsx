d4e4e153bd3bdef3d4abcabac54e80dfffcc8e19
Adam Szreiber <adam.szr98@gmail.com>
2023-07-08 20:15:13 +0200

    fixed nesting (element into element) dropping

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, DragEventHandler, useEffect, useState } from 'react'
+import { CSSProperties, DragEventHandler, useEffect, useState, DragEvent } from 'react'
 // import FormData from 'form-data'
 
 const getItems = (count:number, prefix = `item`) =>
@@ -10,67 +10,42 @@ const getItems = (count:number, prefix = `item`) =>
 const UploadingComponent = () => {
 
 
-  const getDragAfterElement = (container:Element, y:number) => {
-    const draggableElemenets = Array.from( container.querySelectorAll( `.draggable:not(.dragging)` ) )
-    return draggableElemenets.reduce( (closest:any, child) => {
-      const box = child.getBoundingClientRect()
-      const offset = y - box.top - box.height / 2
-      if (offset < 0 && offset > closest.offset)
-        return { offset:offset, element:child }
-
-      return closest
-    }, { offset:Number.NEGATIVE_INFINITY, element:undefined } ).element
+  function allowDrop( ev:DragEvent<HTMLDivElement> ) {
+    ev.preventDefault()
   }
-
-  useEffect( () => {
-    if (!window) return
-
-    const draggableElemenets = document.querySelectorAll( `.draggable` )
-    const containers = document.querySelectorAll( `.container` )
-
-    draggableElemenets.forEach( draggable => {
-
-      draggable.addEventListener( `dragstart`, () => {
-        draggable.classList.add( `dragging` )
-      } )
-
-      draggable.addEventListener( `dragend`, () => {
-        draggable.classList.remove( `dragging` )
-      } )
-
-    } )
-
-    containers.forEach( container => {
-
-      container.addEventListener( `dragover`, e => {
-        e.preventDefault()
-        const afterElement = getDragAfterElement( container, (e  as MouseEvent).clientY )
-        const draggable = document.querySelector( `.dragging` )!
-        if (!afterElement) {
-          container.appendChild( draggable! )
-        } else {
-          console.log({ afterElement })
-          container.insertBefore( draggable, afterElement )
-        }
-      } )
-
-    } )
-  }, [] )
+  
+  function drag( ev:DragEvent<HTMLDivElement> ) {
+    ev.dataTransfer!.setData( `text`, (ev.target as HTMLElement).id )
+  }
+  
+  function drop( ev:DragEvent<HTMLDivElement> ) {
+    const dropInElement = ev.target as HTMLElement
+    ev.preventDefault()
+    const dropInDraggable = dropInElement.className == `draggable`
+    const data = ev.dataTransfer!.getData( `text` )
+    if (dropInDraggable == true) {
+      console.log( dropInElement.parentElement )
+      dropInElement.parentElement!.appendChild( document.getElementById( data )! )
+      return
+    }
+    (ev.target as HTMLElement).appendChild( document.getElementById( data )! )
+  }
+  
 
   return (
     <>
 
       <div className='board' style={{ display:`flex`, gap:`10px` }}>
-        <div className='container' style={{  backgroundColor:`#9e9e9e` }}>
+        <div className='container' id="drag-section-1" style={{  backgroundColor:`#9e9e9e` }} onDrop={e => drop( e )} onDragOver={e => allowDrop( e )}>
           <div className='title' style={{ textAlign:`center` }}> drag-section-1</div>
-          <p className="draggable" draggable>1</p>
-          <p className="draggable" draggable>2</p>
+          <p className="draggable" id="c1-item-1" draggable onDragStart={e => drag( e )}>1</p>
+          <p className="draggable" id="c1-item-2" draggable onDragStart={e => drag( e )}>2</p>
         </div>
 
-        <div className='container' style={{ backgroundColor:`#a67c7c` }}>
+        <div className='container' id="drag-section-2" style={{ backgroundColor:`#a67c7c` }} onDrop={e => drop( e )} onDragOver={e => allowDrop( e )}>
           <div className='title' style={{ textAlign:`center` }}> drag-section-2</div>
-          <p className="draggable" draggable>3</p>
-          <p className="draggable" draggable>4</p>
+          <p className="draggable" id="c2-item-1" draggable onDragStart={e => drag( e )}>3</p>
+          <p className="draggable" id="c2-item-2" draggable onDragStart={e => drag( e )}>4</p>
         </div>
       </div>
 
