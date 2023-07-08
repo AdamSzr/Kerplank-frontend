bd0e82910d589e290ef4c27842e018b8af84691f
Adam Szreiber <adam.szr98@gmail.com>
2023-07-08 20:35:37 +0200

    testing of json passing as data to event

--- pages/test.tsx
+++ pages/test.tsx
@@ -5,36 +5,46 @@ const getItems = (count:number, prefix = `item`) =>
   Array.from( { length:count }, (v, k) => k ).map( k => `${prefix} ${k}` )
 
 
-
-
 const UploadingComponent = () => {
 
 
   function allowDrop( ev:DragEvent<HTMLDivElement> ) {
     ev.preventDefault()
   }
-  
+
   function drag( ev:DragEvent<HTMLDivElement> ) {
-    ev.dataTransfer!.setData( `text`, (ev.target as HTMLElement).id )
-  }
-  
-  function drop( ev:DragEvent<HTMLDivElement> ) {
-    const dropInElement = ev.target as HTMLElement
-    ev.preventDefault()
-    const dropInDraggable = dropInElement.className == `draggable`
-    const data = ev.dataTransfer!.getData( `text` )
-    if (dropInDraggable == true) {
-      console.log( dropInElement.parentElement )
-      dropInElement.parentElement!.appendChild( document.getElementById( data )! )
-      return
+    const z = {
+      "imie": `John`,
+      "nazwisko": `Doe`,
+      "wiek": 30,
+      "plec": `mężczyzna`,
+      "adres": {
+        "ulica": `Main Street`,
+        "miasto": `Nowy Jork`,
+        "kod_pocztowy": `12345`,
+        "kraj": `USA`,
+      },
     }
-    (ev.target as HTMLElement).appendChild( document.getElementById( data )! )
+    ev.dataTransfer!.setData( `text`, (ev.target as HTMLElement).id )
+    ev.dataTransfer!.setData( `person`, JSON.stringify( z ) )
   }
-  
+
+  function drop( ev:DragEvent<HTMLDivElement> ) {
+    const dropIntoElement = ev.target as HTMLElement
+    ev.preventDefault()
+    const dropIntoAnotherDraggableElement = dropIntoElement.className == `draggable`
+    const data = ev.dataTransfer!.getData( `text` )
+    const person = JSON.parse( ev.dataTransfer!.getData( `person` ) )
+    console.log({ person })
+    if (dropIntoAnotherDraggableElement)
+      dropIntoElement.parentElement!.appendChild( document.getElementById( data )! )
+    else
+      dropIntoElement.appendChild( document.getElementById( data )! )
+  }
+
 
   return (
     <>
-
       <div className='board' style={{ display:`flex`, gap:`10px` }}>
         <div className='container' id="drag-section-1" style={{  backgroundColor:`#9e9e9e` }} onDrop={e => drop( e )} onDragOver={e => allowDrop( e )}>
           <div className='title' style={{ textAlign:`center` }}> drag-section-1</div>
@@ -47,7 +57,13 @@ const UploadingComponent = () => {
           <p className="draggable" id="c2-item-1" draggable onDragStart={e => drag( e )}>3</p>
           <p className="draggable" id="c2-item-2" draggable onDragStart={e => drag( e )}>4</p>
         </div>
+        <div className='container' id="drag-section-3" style={{ backgroundColor:`#a67c7c` }} onDrop={e => drop( e )} onDragOver={e => allowDrop( e )}>
+          <div className='title' style={{ textAlign:`center` }}> drag-section-3</div>
+          <p className="draggable" id="c3-item-1" draggable onDragStart={e => drag( e )}>5</p>
+          <p className="draggable" id="c3-item-2" draggable onDragStart={e => drag( e )}>6</p>
+        </div>
       </div>
+      
 
     </>
   )
